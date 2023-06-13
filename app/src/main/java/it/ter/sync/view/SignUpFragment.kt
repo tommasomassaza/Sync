f2e5ab57b43b1a76d8308aa274346398f2e603c0
tommasomassaza <tommaso.massaza@edu.unito.it>
2023-06-13 23:43:57 +0200

    Fragment Account

--- app/src/main/java/it/ter/sync/view/SignUpFragment.kt
+++ app/src/main/java/it/ter/sync/view/SignUpFragment.kt
@@ -6,13 +6,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ArrayAdapter
-import android.widget.EditText
-import android.widget.Spinner
 import android.widget.Toast
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
-import com.google.android.material.button.MaterialButton
 import it.ter.sync.R
 import it.ter.sync.databinding.FragmentSignupBinding
 import it.ter.sync.viewmodel.UserViewModel
