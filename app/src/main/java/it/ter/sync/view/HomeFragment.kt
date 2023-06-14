4a56eae13572794746114839f7c0dc4468a39534
tommasomassaza <tommaso.massaza@edu.unito.it>
2023-06-14 15:06:11 +0200

    Home Fragment con Adapter

--- app/src/main/java/it/ter/sync/view/HomeFragment.kt
+++ app/src/main/java/it/ter/sync/view/HomeFragment.kt
@@ -1,27 +1,30 @@
 package it.ter.sync.view
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
-import android.widget.Toast
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.google.android.material.navigation.NavigationView
-import com.google.firebase.auth.FirebaseAuth
 import it.ter.sync.R
 import it.ter.sync.databinding.FragmentHomeBinding
+import it.ter.sync.view.adapter.PostAdapter
 import it.ter.sync.viewmodel.HomeViewModel
 import it.ter.sync.viewmodel.UserViewModel
 
 class HomeFragment : Fragment() {
     private val TAG: String = javaClass.simpleName
-    private val homeViewModel: HomeViewModel by activityViewModels()
     private val userViewModel: UserViewModel by activityViewModels()
+    private val homeViewModel: HomeViewModel by activityViewModels()
     private var _binding: FragmentHomeBinding? = null
+
+    private lateinit var recyclerView: RecyclerView
+
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -34,6 +37,12 @@ class HomeFragment : Fragment() {
     ): View {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
+
+        homeViewModel.getAllUsers()
+
+        recyclerView = binding.recyclerPost
+        val layoutManager = LinearLayoutManager(requireContext())
+        recyclerView.layoutManager = layoutManager
 
         initObservers()
 
@@ -58,14 +67,13 @@ class HomeFragment : Fragment() {
      * it initializes the view model and the methods used to retrieve the live data for the interface
      */
     private fun initObservers() {
-        val textView: TextView = binding.textHome
-        homeViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
+        Log.i(TAG, "Registering Observers: ViewModel? $homeViewModel")
+        homeViewModel.users.observe(viewLifecycleOwner) {
+
+            val adapter = PostAdapter(it)
+            recyclerView.adapter = adapter
         }
 
-
-
     }
-
 
 }
