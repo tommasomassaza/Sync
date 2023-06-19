0c398688f628714346f3d57c19283e425b465448
tommasomassaza <tommaso.massaza@edu.unito.it>
2023-06-19 23:06:45 +0200

    FireBase

--- app/src/main/java/it/ter/sync/viewmodel/MessageViewModel.kt
+++ app/src/main/java/it/ter/sync/viewmodel/MessageViewModel.kt
@@ -9,7 +9,6 @@ import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
-import com.google.firebase.database.ServerValue
 import com.google.firebase.database.ValueEventListener
 import it.ter.sync.database.message.MessageData
 import kotlinx.coroutines.Dispatchers
@@ -22,7 +21,7 @@ class MessageViewModel(application: Application) : AndroidViewModel(application)
     private var TAG = this::class.simpleName
 
     private val firebaseAuth: FirebaseAuth = FirebaseAuth.getInstance()
-    private val database: FirebaseDatabase = FirebaseDatabase.getInstance("https://sync-58606-default-rtdb.europe-west1.firebasedatabase.app/")
+    private val database: FirebaseDatabase = FirebaseDatabase.getInstance()
 
 
     var messageList: MutableLiveData<List<MessageData>> = MutableLiveData()
