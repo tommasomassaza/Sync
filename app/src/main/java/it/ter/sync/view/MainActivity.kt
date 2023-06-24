10f43201951759f1eb35f563a412aca7d8b2034a
ettoregiorgionew <120247471+ettoregiorgionew@users.noreply.github.com>
2023-06-24 21:40:09 +0200

    Aggiunta immagine al profilo

--- app/src/main/java/it/ter/sync/view/MainActivity.kt
+++ app/src/main/java/it/ter/sync/view/MainActivity.kt
@@ -1,12 +1,8 @@
 package it.ter.sync.view
 
 import android.os.Bundle
-import android.util.Log
 import android.view.Menu
-import android.view.MenuItem
 import android.widget.Toast
-import androidx.activity.viewModels
-import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.navigation.NavigationView
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
@@ -16,14 +12,27 @@ import androidx.navigation.ui.setupWithNavController
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.GravityCompat
-import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.NavController
-import androidx.navigation.NavDestination
-import androidx.navigation.ui.NavigationUI
 import it.ter.sync.R
 import it.ter.sync.databinding.ActivityMainBinding
 import it.ter.sync.viewmodel.UserViewModel
+
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+
+import android.provider.MediaStore
+import android.util.Log
+import android.view.View
+import android.widget.ImageButton
+import android.widget.ImageView
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.cardview.widget.CardView
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
     private val TAG: String = javaClass.simpleName
@@ -33,11 +42,29 @@ class MainActivity : AppCompatActivity() {
     private lateinit var navController: NavController
     private lateinit var drawerLayout: DrawerLayout
 
+
+
+
+    private lateinit var imageView: ImageView
+    private lateinit var cardView: CardView
+    private lateinit var imagePickerLauncher: ActivityResultLauncher<String>
+
+    private val PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE = 1
+
+
+    companion object {
+        val IMAGE_REQUEST_CODE = 1_000;
+    }
+
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
 
         userViewModel = ViewModelProvider(this).get(UserViewModel::class.java)
 
@@ -49,15 +76,57 @@ class MainActivity : AppCompatActivity() {
         navController = findNavController(R.id.nav_host_fragment_content_main)
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
+
+
+
+        val headerView = navView.getHeaderView(0)
+        imageView = headerView.findViewById(R.id.imageButton)
+        cardView = headerView.findViewById(R.id.cardView)
+
         appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.homeFragment, R.id.accountFragment, R.id.chatFragment, R.id.loginFragment
+                R.id.homeFragment, R.id.accountFragment, R.id.loginFragment
             ), drawerLayout
         )
 
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+
+        //imageButton = findViewById(R.id.imageButton)
+
+        imagePickerLauncher = registerForActivityResult(ActivityResultContracts.GetContent()) { imageUri ->
+            // Aggiungi il codice per visualizzare l'immagine nell'ImageView
+            imageView.setImageURI(imageUri)
+        }
+
+
+        imageView.setOnClickListener {
+            pickImageFromGallery()
+        }
+
     }
+
+    private fun pickImageFromGallery() {
+        val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+        startActivityForResult(intent, 101)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == RESULT_OK && requestCode == 101) {
+            val imageUri = data?.data
+            val bitmap = MediaStore.Images.Media.getBitmap(contentResolver, imageUri)
+            imageView.setImageBitmap(bitmap)
+        }
+    }
+
+
+
+
+
+
+
+
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -70,7 +139,7 @@ class MainActivity : AppCompatActivity() {
 
         // Se mi trovo nel loginFragment non posso accedere al menù
         val currentFragment = navController.currentDestination
-        if(currentFragment?.id == R.id.loginFragment){
+        if (currentFragment?.id == R.id.loginFragment) {
             return false
         }
 
@@ -85,4 +154,6 @@ class MainActivity : AppCompatActivity() {
             super.onBackPressed()
         }
     }
+
 }
+
